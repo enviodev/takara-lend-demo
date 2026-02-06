dd6c996b4619b0765c9ddcfcbf03a4aa6ee46450
denhampreen <denhampreen@gmail.com>
2026-02-06 11:49:03 +0200

    terc20 indexer

---
+++ src/EventHandlers.ts
@@ -0,0 +1,92 @@
+import {
+  TERC20Delegator,
+  Token,
+  TokenAccount,
+  Approval,
+} from "generated";
+
+const tokenAccountId = (tokenAddress: string, accountAddress: string) =>
+  `${tokenAddress}-${accountAddress}`;
+const approvalId = (tokenAddress: string, owner: string, spender: string) =>
+  `${tokenAddress}-${owner}-${spender}`;
+
+TERC20Delegator.Approval.handler(async ({ event, context }) => {
+  const tokenAddress = event.srcAddress.toLowerCase();
+  const owner = event.params.owner.toString();
+  const spender = event.params.spender.toString();
+  const amount = event.params.amount;
+
+  await context.Token.getOrCreate({ id: tokenAddress });
+
+  let ownerAccount = await context.Account.get(owner);
+  if (ownerAccount === undefined) {
+    context.Account.set({ id: owner });
+  }
+
+  let spenderAccount = await context.Account.get(spender);
+  if (spenderAccount === undefined) {
+    context.Account.set({ id: spender });
+  }
+
+  const approval: Approval = {
+    id: approvalId(tokenAddress, owner, spender),
+    token_id: tokenAddress,
+    amount,
+    owner_id: owner,
+    spender_id: spender,
+  };
+  context.Approval.set(approval);
+});
+
+TERC20Delegator.Transfer.handler(async ({ event, context }) => {
+  const tokenAddress = event.srcAddress.toLowerCase();
+  const from = event.params.from.toString();
+  const to = event.params.to.toString();
+  const amount = event.params.amount;
+
+  await context.Token.getOrCreate({ id: tokenAddress });
+
+  const fromTokenAccountId = tokenAccountId(tokenAddress, from);
+  let fromTokenAccount = await context.TokenAccount.get(fromTokenAccountId);
+
+  if (fromTokenAccount === undefined) {
+    context.TokenAccount.set({
+      id: fromTokenAccountId,
+      token_id: tokenAddress,
+      account_id: from,
+      balance: 0n - amount,
+    });
+  } else {
+    context.TokenAccount.set({
+      ...fromTokenAccount,
+      balance: fromTokenAccount.balance - amount,
+    });
+  }
+
+  let fromAccount = await context.Account.get(from);
+  if (fromAccount === undefined) {
+    context.Account.set({ id: from });
+  }
+
+  const toTokenAccountId = tokenAccountId(tokenAddress, to);
+  let toTokenAccount = await context.TokenAccount.get(toTokenAccountId);
+
+  if (toTokenAccount === undefined) {
+    context.TokenAccount.set({
+      id: toTokenAccountId,
+      token_id: tokenAddress,
+      account_id: to,
+      balance: amount,
+    });
+  } else {
+    context.TokenAccount.set({
+      ...toTokenAccount,
+      balance: toTokenAccount.balance + amount,
+    });
+  }
+
+  let toAccount = await context.Account.get(to);
+  if (toAccount === undefined) {
+    context.Account.set({ id: to });
+  }
+});
---
+++ test/test.ts
@@ -0,0 +1,54 @@
+import assert from "assert";
+import { TestHelpers, TokenAccount } from "generated";
+const { MockDb, TERC20Delegator, Addresses } = TestHelpers;
+
+describe("Transfers", () => {
+  it("Transfer subtracts from sender TokenAccount balance and adds to receiver TokenAccount balance", async () => {
+    const mockDbEmpty = MockDb.createMockDb();
+
+    const tokenAddress = Addresses.defaultAddress;
+    const userAddress1 = Addresses.mockAddresses[0];
+    const userAddress2 = Addresses.mockAddresses[1];
+
+    const tokenAccount1Id = `${tokenAddress}-${userAddress1}`;
+    const mockTokenAccount: TokenAccount = {
+      id: tokenAccount1Id,
+      token_id: tokenAddress,
+      account_id: userAddress1,
+      balance: 5n,
+    };
+
+    const mockDb = mockDbEmpty.entities.TokenAccount.set(mockTokenAccount);
+
+    const mockTransfer = TERC20Delegator.Transfer.createMockEvent({
+      from: userAddress1,
+      to: userAddress2,
+      amount: 3n,
+      mockEventData: { srcAddress: tokenAddress },
+    });
+
+    const mockDbAfterTransfer = await TERC20Delegator.Transfer.processEvent({
+      event: mockTransfer,
+      mockDb,
+    });
+
+    const account1TokenBalance =
+      mockDbAfterTransfer.entities.TokenAccount.get(tokenAccount1Id)?.balance;
+
+    assert.equal(
+      2n,
+      account1TokenBalance,
+      "Should have subtracted transfer amount 3 from userAddress1 balance 5",
+    );
+
+    const tokenAccount2Id = `${tokenAddress}-${userAddress2}`;
+    const account2TokenBalance =
+      mockDbAfterTransfer.entities.TokenAccount.get(tokenAccount2Id)?.balance;
+
+    assert.equal(
+      3n,
+      account2TokenBalance,
+      "Should have added transfer amount 3 to userAddress2 balance 0",
+    );
+  });
+});
